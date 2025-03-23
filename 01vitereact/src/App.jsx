e9f50b8dcf464d5ca6a9ef1e58eb574ea68b9bbd
Harsshad Pawar <pawar.harshad186@gmail.com>
2025-03-23 20:12:16 +0400

    3rd session practise changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,18 @@
 import Chai from "./chai.jsx";
 function App() {
-  return <Chai />;
+  const username = "chai aur code";
+  return (
+    <>
+      <Chai />
+      <h1> chai aur react {username}</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ut,
+        dignissimos vero fugiat nemo, ex quidem iure error mollitia tenetur
+        incidunt quibusdam iusto delectus suscipit ratione quo aut, eligendi
+        atque?
+      </p>
+    </>
+  );
 }
 
 export default App;
